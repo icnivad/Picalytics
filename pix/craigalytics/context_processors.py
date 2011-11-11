34d1b5caaefaab7efcf11487337dc8280a97b0fa
Ben Earle <bearle2009@gmail.com>
2011-11-11 04:13:49 -0500

    quick

--- pix/craigalytics/context_processors.py
+++ pix/craigalytics/context_processors.py
@@ -1,3 +1,3 @@
 def get_useful_constants(request):
     from django.conf import settings
-    return {'MEDIA_URL': settings.MEDIA_URL, 'SUBSITE_URL':settings.SUBSITE_URL}
+    return {'MEDIA_URL': settings.MEDIA_URL}
