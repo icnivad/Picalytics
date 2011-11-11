772655a4af46b9277148856753b7b3f295105ad2
Ben Earle <bearle2009@gmail.com>
2011-11-11 03:07:26 -0500

    quick

--- pix/urls.py
+++ pix/urls.py
@@ -1,7 +1,21 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.conf import settings
-import suburls
+import settings
 
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+	
 urlpatterns = patterns('',
-    (r'^%s' % settings.SUBSITE , include(suburls))
+    # Examples:
+    # url(r'^$', 'pix.views.home', name='home'),
+    (r'^Media/(?P<path>.*)$', 'django.views.static.serve',  {'document_root': settings.MEDIA_ROOT}),
+    url(r'^email/', include('pix.email_tracker.urls')),
+    url(r'^my_images/', include('pix.craigalytics.urls')),
+    url(r'^$', 'main.views.main', name='main'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
 )
