0789f4577458225a37048e55b833f44066b73bf6
Ben Earle <bearle2009@gmail.com>
2011-11-11 03:22:29 -0500

    quick

--- pix/urls.py
+++ pix/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 import settings
-
+x
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
